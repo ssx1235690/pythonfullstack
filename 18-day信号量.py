bd560f99f69f88c203dc612b43d58828f9334c72
xy <295312998@qq.com>
2018-05-13 20:54:12 +0800

    18-days

--- 18-day信号量.py
+++ 18-day信号量.py
@@ -4,3 +4,31 @@
 # __Author__ = songxy
 # date : 2018/5/12
 
+import threading,time,sys
+
+class Account:
+    def __init__(self,money,name,r):
+        self.blance = money
+        self.name = name
+
+    def withdraw(self,num):
+        se
+        self.blance -= num
+    def repay(self,num):
+        self.blance += num
+
+song = Account(99999,'songshenxiang')
+print(song.blance)
+
+def caozuo(_from,to,count):
+    # r = threading.Lock()
+    # r.acquire()
+    _from.withdraw(count)
+    to.repay(count)
+    # r.release()
+a1 = Account(2000,'song')
+a2 = Account(1000,'xiang')
+
+caozuo(a1,a2,500)
+print(a1.blance)
+print(a2.blance)
