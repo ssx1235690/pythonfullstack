b654d1e4acefdecb2c80bbc8f607a0e7d322ce99
xy <295312998@qq.com>
2018-05-13 23:52:06 +0800

    18-days

--- 18-day多线程利器queue.py
+++ 18-day多线程利器queue.py
@@ -34,3 +34,141 @@
 # q.put_nowait(item) 相当q.put(item, False)
 # q.task_done() 在完成一项工作之后，q.task_done() 函数向任务已经完成的队列发送一个信号
 # q.join() 实际上意味着等到队列为空，再执行别的操作
+
+import queue
+#FIFO
+d = queue.Queue(maxsize=10)
+d.put('song')
+print(d.get())
+
+
+###### 例子  ########
+# import threading,queue
+# from time import sleep
+# from random import randint
+# class Production(threading.Thread):
+#     def run(self):
+#         while True:
+#             r=randint(0,100)
+#             q.put(r)
+#             print("生产出来%s号包子"%r)
+#             sleep(1)
+# class Proces(threading.Thread):
+#     def run(self):
+#         while True:
+#             re=q.get()
+#             print("吃掉%s号包子"%re)
+# if __name__=="__main__":
+#     q=queue.Queue(10)
+#     threads=[Production(),Production(),Production(),Proces()]
+#     for t in threads:
+#         t.start()
+
+
+
+
+
+##### 列子2 ##########
+# import time,random
+# import queue,threading
+# q = queue.Queue()
+# def Producer(name):
+#   count = 0
+#   while count <20:
+#     time.sleep(random.randrange(3))
+#     q.put(count)
+#     print('Producer %s has produced %s baozi..' %(name, count))
+#     count +=1
+# def Consumer(name):
+#   count = 0
+#   while count <20:
+#     time.sleep(random.randrange(4))
+#     if not q.empty():
+#         data = q.get()
+#         print(data)
+#         print('\033[32;1mConsumer %s has eat %s baozi...\033[0m' %(name, data))
+#     else:
+#         print("-----no baozi anymore----")
+#     count +=1
+# p1 = threading.Thread(target=Producer, args=('A',))
+# c1 = threading.Thread(target=Consumer, args=('B',))
+# p1.start()
+# c1.start()
+
+
+
+##### 例子3 ##############
+#实现一个线程不断生成一个随机数到一个队列中(考虑使用Queue这个模块)
+# 实现一个线程从上面的队列里面不断的取出奇数
+# 实现另外一个线程从上面的队列里面不断取出偶数
+
+# import random,threading,time
+# from queue import Queue
+# #Producer thread
+# class Producer(threading.Thread):
+#   def __init__(self, t_name, queue):
+#     threading.Thread.__init__(self,name=t_name)
+#     self.data=queue
+#   def run(self):
+#     for i in range(10):  #随机产生10个数字 ，可以修改为任意大小
+#       randomnum=random.randint(1,99)
+#       print ("%s: %s is producing %d to the queue!" % (time.ctime(), self.getName(), randomnum))
+#       self.data.put(randomnum) #将数据依次存入队列
+#       time.sleep(1)
+#     print ("%s: %s finished!" %(time.ctime(), self.getName()))
+#
+# #Consumer thread
+# class Consumer_even(threading.Thread):
+#   def __init__(self,t_name,queue):
+#     threading.Thread.__init__(self,name=t_name)
+#     self.data=queue
+#   def run(self):
+#     while 1:
+#       try:
+#         val_even = self.data.get(1,5) #get(self, block=True, timeout=None) ,1就是阻塞等待,5是超时5秒
+#         if val_even%2==0:
+#           print ("%s: %s is consuming. %d in the queue is consumed!" % (time.ctime(),self.getName(),val_even))
+#           time.sleep(2)
+#         else:
+#           self.data.put(val_even)
+#           time.sleep(2)
+#       except:   #等待输入，超过5秒 就报异常
+#         print ("%s: %s finished!" %(time.ctime(),self.getName()))
+#         break
+# class Consumer_odd(threading.Thread):
+#   def __init__(self,t_name,queue):
+#     threading.Thread.__init__(self, name=t_name)
+#     self.data=queue
+#   def run(self):
+#     while 1:
+#       try:
+#         val_odd = self.data.get(1,5)
+#         if val_odd%2!=0:
+#           print ("%s: %s is consuming. %d in the queue is consumed!" % (time.ctime(), self.getName(), val_odd))
+#           time.sleep(2)
+#         else:
+#           self.data.put(val_odd)
+#           time.sleep(2)
+#       except:
+#         print ("%s: %s finished!" % (time.ctime(), self.getName()))
+#         break
+# #Main thread
+# def main():
+#   queue = Queue()
+#   producer = Producer('Pro.', queue)
+#   consumer_even = Consumer_even('Con_even.', queue)
+#   consumer_odd = Consumer_odd('Con_odd.',queue)
+#   producer.start()
+#   consumer_even.start()
+#   consumer_odd.start()
+#   producer.join()
+#   consumer_even.join()
+#   consumer_odd.join()
+#   print ('All threads terminate!')
+#
+# if __name__ == '__main__':
+#   main()
+
+
+# import os
+# print((os.system('dir'))
