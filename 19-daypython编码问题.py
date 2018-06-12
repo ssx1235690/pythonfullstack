1a9cb0d99a71617ba27d1dd030585b22c172f96d
xy <295312998@qq.com>
2018-06-13 00:00:31 +0800

    19-days

--- 19-daypython编码问题.py
+++ 19-daypython编码问题.py
@@ -2,8 +2,44 @@
 # -*- coding: utf-8 -*-
 # __Author__ = songxy
 # date : 2018/6/4
+#
 
-f = open('README.md','rb')
-print(str(f.read(),'utf8'))
+# http://www.cnblogs.com/yuanchenqi/articles/5938733.html
+
+
+# f = open('README.md','r',encoding='utf8')
+# print(str(f.read()))
+
+# python 2 下执行下列参数
+
+# print('乐乐爱爸爸')
+# print(['乐乐爱爸爸'])
+# song = '乐乐爱爸爸'
+# print type(song)
+# print len(song)
+# song = u'乐乐爱爸爸'
+# # 乐乐爱爸爸
+# # ['\xe4\xb9\x90\xe4\xb9\x90\xe7\x88\xb1\xe7\x88\xb8\xe7\x88\xb8']
+# # <type 'str'>
+# # 15
+# print type(song)
+# print len(song)
+# # <type 'unicode'>
+# # 5
+
+# python3 下执行
 
 print('乐乐爱爸爸')
+print(['乐乐爱爸爸'])
+song = '乐乐爱爸爸'
+print (type(song))
+print (len(song))
+# 乐乐爱爸爸
+# ['乐乐爱爸爸']
+# <class 'str'>
+# 5
+song = u'乐乐爱爸爸'
+print(type(song))
+print(len(song))
+# <class 'str'>
+# 5
