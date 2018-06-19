c3535990e3de621e1985ffcb63443a95ef3da5e9
xy <295312998@qq.com>
2018-06-20 00:25:23 +0800

    20-days

--- 20-day协程gevent模块.py
+++ 20-day协程gevent模块.py
@@ -49,3 +49,23 @@ gevent.joinall([
     gevent.spawn(func2),
     # gevent.spawn(func3),
 ])
+
+from gevent import monkey;
+
+monkey.patch_all()
+import gevent
+from  urllib.request import urlopen
+
+
+def f(url):
+    print('GET: %s' % url)
+    resp = urlopen(url)
+    data = resp.read()
+    print('%d bytes received from %s.' % (len(data), url))
+
+
+gevent.joinall([
+    gevent.spawn(f, 'https://www.python.org/'),
+    gevent.spawn(f, 'https://www.yahoo.com/'),
+    gevent.spawn(f, 'https://github.com/'),
+])
