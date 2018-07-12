b9334e4778e0eab7ce0742f677ec49427b5d48c1
ronglian <sxsong@ronglian.com>
2018-07-12 10:16:07 +0800

    22-day

--- 22-daySelectClient.py
+++ 22-daySelectClient.py
@@ -4,7 +4,15 @@
 # date : 2018/7/11
 
 import socket
-
-sk = socket.socket()
-sk.connect('127.0.0.1',12452)
 while 1:
+    sk1 = socket.socket()
+    sk1.connect(('127.0.0.1',12452))
+
+    song = sk1.recv(1024)
+    print(song.decode('utf8'))
+    print("++=========+++")
+    sk2 = socket.socket()
+    sk2.connect(('127.0.0.1',12451))
+
+    song = sk2.recv(1024)
+    print(song.decode('utf8'))
--- 22-daySelectServer.py
+++ 22-daySelectServer.py
@@ -6,13 +6,27 @@
 import socket,select
 
 sk1 = socket.socket()
-sk1.bind('127.0.0.1',12451)
+sk1.bind(('127.0.0.1',12451))
 sk1.listen(2)
 sk2 = socket.socket()
-sk1.bind('127.0.0.1',12452)
+sk2.bind(('127.0.0.1',12452))
 sk2.listen(2)
+while True:
+    r,w,e = select.select([sk1,sk2],[],[])
 
-r,w,s = select([sk1,sk2],[],[])
-for i in r:
-    conn,addr = i.accept()
-    conn.send(bytes('song',encoding='gbk'))
+    print("++========================++")
+    print(r)
+
+
+    for i in r:
+        conn,addr = i.accept()
+        print("+++++===========addr and  conn  的值============++++++++")
+
+        print(addr)
+        print(conn)
+
+        print("+++++=======================================++++++++")
+
+
+        song = input('gei me a password')
+        conn.send(song.encode('utf8'))
