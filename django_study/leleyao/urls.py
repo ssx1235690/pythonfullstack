94858ce4151aed7f3b266e7d15e15e7b76fe2359
song-pc <295312998@qq.com>
2019-01-27 23:28:03 +0800

    33-day  django

--- django_study/leleyao/urls.py
+++ django_study/leleyao/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from leleyao import views
 urlpatterns = [
+   path('index.html',views.sxsong),
    path('leleyao/',views.leleyao)
 ]
--- django_study/leleyao/views.py
+++ django_study/leleyao/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from leleyao import models
 
 # Create your views here.
 def sxsong(request):
@@ -7,4 +8,7 @@ def sxsong(request):
     return HttpResponse('sxsong is my father')
 def leleyao(request):
     print('xxxxx')
-    return HttpResponse('sxsong is my father')
+    xxx=models.Author()
+    xxx.name='leleyao'
+    xxx.save()
+    return HttpResponse('qing is my mother')
