493c5309186f957b1414990825119512e56190ba
song-pc <295312998@qq.com>
2019-03-28 14:45:03 +0800

    34-day rabbitmq

--- 消息队列/1.rabbit-send.py
+++ 消息队列/1.rabbit-send.py
@@ -3,15 +3,13 @@ class send():
     def __init__(self,que_nam='hello'):
         self.credentials = pika.PlainCredentials('mytest', 'mytest')
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.157.132',credentials=self.credentials))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=que_nam,durable=True) #只是指出队列持久化
     def action(self,thread_num,msgj='ndyd 996'):
         que_nam='hello'
         print(thread_num)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=que_nam, durable=True)  # 只是指出队列持久化
         while True:
             try:
-                self.channel = self.connection.channel()
-                self.channel.queue_declare(queue=que_nam, durable=True)  # 只是指出队列持久化
                 self.channel.basic_publish(exchange='',
                                   routing_key='hello',
                                   body=msgj,
@@ -19,11 +17,11 @@ class send():
                                            )
             except BaseException as e:
                 print(e)
-            print(" [x] Sent %s" %msgj)
-            time.sleep(0.1)
+            print(" [x] Sent %s %d" %(msgj,thread_num))
+            # time.sleep(0.001)
     def lll(self):
         li=[]
-        for i in range(1,10):
+        for i in range(1,2):
             th=threading.Thread(target=self.action,args=(i,))
             th.start()
             li.append(th)
