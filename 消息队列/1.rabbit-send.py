42bc3421c489d7a95317635e310b6d525c5749ae
song-pc <295312998@qq.com>
2019-03-28 13:48:16 +0800

    34-day rabbitmq

--- 消息队列/1.rabbit-send.py
+++ 消息队列/1.rabbit-send.py
@@ -5,23 +5,33 @@ class send():
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.157.132',credentials=self.credentials))
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=que_nam,durable=True) #只是指出队列持久化
-    def action(self,msgj='ndyd 996'):
+    def action(self,thread_num,msgj='ndyd 996'):
+        que_nam='hello'
+        print(thread_num)
         while True:
-            self.channel.basic_publish(exchange='',
-                              routing_key='hello',
-                              body=msgj,
-                               properties=pika.BasicProperties(delivery_mode=2)
-                                       )
-        print(" [x] Sent %s" %msgj)
-        time.sleep(0.5)
+            try:
+                self.connection = pika.BlockingConnection(
+                    pika.ConnectionParameters(host='192.168.157.132', credentials=self.credentials))
+                self.channel = self.connection.channel()
+                self.channel.queue_declare(queue=que_nam, durable=True)  # 只是指出队列持久化
+                self.channel.basic_publish(exchange='',
+                                  routing_key='hello',
+                                  body=msgj,
+                                   properties=pika.BasicProperties(delivery_mode=2)
+                                           )
+            except BaseException as e:
+                print(e)
+            print(" [x] Sent %s" %msgj)
+            time.sleep(0.001)
     def lll(self):
         li=[]
-        for i in range(1,1000):
-            th=threading.Thread(target=self.action())
+        for i in range(1,10):
+            th=threading.Thread(target=self.action,args=(i,))
             th.start()
             li.append(th)
-        for i in li:
-            i.join()
+        # for i in li:
+        #     i.join()
         # connection.close()
-song=send()
+song=send('sdfl')
 song.lll()
+# song.action(1)
