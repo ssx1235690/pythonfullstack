e53165f50734410a8024e820f757011db4089a4b
song-pc <295312998@qq.com>
2019-03-31 12:54:43 +0800

    34-day redis

--- test_dom/1.class.py
+++ test_dom/1.class.py
@@ -15,13 +15,34 @@ class song():
         print(self.name)
 
 class lele(song):
+    xxxxx='123cccc'
     def __init__(self):
         print(self._xxx)
         try:
-            print(super().__xxx2)
+            self.sss()
+            print(super()._xxx2)
         except BaseException as e:
             print(e)
+    @staticmethod # 名义上归类来管理
+    def fun1_static():
+        print('static')
+    @classmethod   #类方法只能访问类变量，不能访问实例变量
+    def fun2_class(cls):
+        print(cls.xxxxx)
+    @property         #把一个方法变成一个静态属性
+    def  fun3_propp(self):
+        print('xxxx',self.propp_song)
+    @fun3_propp.setter
+    def propp_setter(self,song):
+        print(song)
+        self.propp_song = song
+
+
 
 
 l1 = song()
 l2 = lele()
+l2.propp_setter='sdfsdfsdfs'
+l2.fun3_propp
+
+
