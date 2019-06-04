728f56b3788d6792c7729775e51f59dc6c0c32c1
workspace <sxsong@cnpc>
2019-06-04 17:38:54 +0800

    37day  while else

---
+++ 20190525New/09-day/1.while语句.py
@@ -0,0 +1,14 @@
+
+# -*- coding: utf-8 -*-
+# @Time    :  2019/6/4 17:36
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 1.while语句.py
+
+flag = 10066
+while flag:
+    if flag > 1000:
+        break
+    flag -= 1
+else:
+    print('while is ok')
