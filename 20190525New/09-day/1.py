3559cedb1e6d6172a45ea64bce41e05dfc23f8ac
workspace <sxsong@cnpc>
2019-06-04 18:44:24 +0800

    37day  计算位数

---
+++ 20190525New/09-day/1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2019/6/4 18:39
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 1.py
+
+#### 计数位数 脚本
+num = input('give me a num ')
+num = int(num)
+flag = 10
+wei = 0
+while num > 1 :
+    num = num // 10
+    wei += 1
+else:
+    print(wei)
