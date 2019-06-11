1bfab654064690abdd96274357ad7364eeb99c4e
workspace <sxsong@cnpc>
2019-06-11 22:45:58 +0800

    11day  list

--- 20190525New/11-day/1.list.py
+++ 20190525New/11-day/1.list.py
@@ -3,3 +3,24 @@
 # @Author  : songxy
 # @Email   : ......998@qq.com
 # @File    : 1.list.py
+
+#####################内置数据结构
+
+import math
+# 数值型
+#  int  float  complex  bool
+# 序列对象
+#  list str truple
+# 键值对
+#  set  dict
+
+
+# print(bool(''))
+
+print(round(2,5))
+print(round(2,6))
+
+print(math.floor(2.5),math.ceil(-2.5))
+print(math.floor(-2.5),math.ceil(2.5))
+
+print(int(2.6),int(-2.6))
