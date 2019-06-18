00eb7710ceeb2b0a6c999af83c7fbe42823bea5b
song-pc <295312998@qq.com>
2019-06-19 00:00:00 +0800

    tuple and ascci

--- 20190525New/12-day/1.杨辉三角.py
+++ 20190525New/12-day/1.杨辉三角.py
@@ -6,11 +6,40 @@
 
 import math
 
+
+# 判断素数
 num = int(input('give me your num:  '))
 list = []
-for i in range(math.ceil(num*0.5),num):
+for i in range(2,math.ceil(num*0.5)):
     if num % i == 0:
         break
+else:
+    print(num)
+
+# 求100以内素数的和
+sum = 0
+for i in range (1,100):
+    for j in range(2,math.ceil(i * 0.5)):
+        if i% j == 0:
+            break
     else:
-        print(num)
-        break
+        sum += i
+else:
+    print(sum)
+
+
+############## 元组 tuple
+
+# t1 = ([1,2],23,342)
+# t1[0][1] = 0
+
+# l1 = [(2,3),89234,234]
+# l1[0][1] = 5
+
+
+########## name tuple
+import collections
+
+t2  = collections.namedtuple('t2',['x','y'])
+zz = t2(1,2)
+print(zz.x)
--- 20190525New/12-day/1.随机数和元组.py
+++ 20190525New/12-day/1.随机数和元组.py
@@ -3,3 +3,14 @@
 # @Author  : songxy
 # @Email   : 2953xx998@qq.com
 # @File    : 1.随机数和元组.py
+
+
+import  random
+print(random.randint(1,6)) # 返回[a,b] 之间的数字
+print(random.randrange(1,6)) # 返回 [a,b) 之间的数字  但是可以指定步长
+
+print(random.choice([1,2,4,5,2,4,5,5,23]))  # 选出一个
+print(random.sample([1,2,4,5,2,4,5,5,23], 2))  # 选出指定个
+
+
+print(random.shuffle([1,2,3,4,5]))  # 就地打乱，没有返回值
