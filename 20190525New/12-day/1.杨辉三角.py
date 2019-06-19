f113c728d08f10db6c716c51747b440e5b8741db
workspace <sxsong@cnpc>
2019-06-19 17:31:16 +0800

    11day   tuple  move

--- 20190525New/12-day/1.杨辉三角.py
+++ 20190525New/12-day/1.杨辉三角.py
@@ -28,18 +28,4 @@ else:
     print(sum)
 
 
-############## 元组 tuple
-
-# t1 = ([1,2],23,342)
-# t1[0][1] = 0
-
-# l1 = [(2,3),89234,234]
-# l1[0][1] = 5
-
-
-########## name tuple
-import collections
-
-t2  = collections.namedtuple('t2',['x','y'])
-zz = t2(1,2)
-print(zz.x)
+#
--- 20190525New/12-day/1.随机数和元组.py
+++ 20190525New/12-day/1.随机数和元组.py
@@ -14,3 +14,19 @@ print(random.sample([1,2,4,5,2,4,5,5,23], 2))  # 选出指定个
 
 
 print(random.shuffle([1,2,3,4,5]))  # 就地打乱，没有返回值
+
+############# 元组 tuple
+
+# t1 = ([1,2],23,342)
+# t1[0][1] = 0
+
+# l1 = [(2,3),89234,234]
+# l1[0][1] = 5
+
+
+########## name tuple
+import collections
+
+t2  = collections.namedtuple('t2',['x','y'])
+zz = t2(1,2)
+print(zz.x)
