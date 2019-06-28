c1da99b3e4d2c4ca7dc11e00fac4224f494cb8ef
inThecompany <sxsong@cnpc>
2019-06-29 07:46:03 +0800

    traversal function args

--- 20190525New/18-days/2.可变参数.py
+++ 20190525New/18-days/2.可变参数.py
@@ -13,7 +13,7 @@ song(1,2,3,4,5,b=8,c=8)
 # song(b=8,c=8,1,2,3,4,5)
 # SyntaxError: positional argument follows keyword argument
 
-# 参数的位置 总结为   普通参数  > 可变位置参数 *x  > 可变关键字参数  **y
+# 参数的位置 总结为   普通参数 > 缺省参数 > 可变位置参数 *x  > keyword-only 参数 >可变关键字参数  **y
 
 
 #
@@ -23,3 +23,32 @@ song(1,2,3,4,5,b=8,c=8)
 #     def song(**sss,x):
 #                    ^
 # SyntaxError: invalid syntax
+
+
+#####################################
+####################################  参数解构
+
+def song(x,y):
+    print(x,y)
+song(*(1,2))
+song(*[1,2])
+
+song(*range(2))
+
+
+#### 结构字典时   key必须为 字符串  变量名必须合法
+
+def song(**kwargs):
+    print(kwargs)
+
+song(**{'sdf':123,'sdfasdf':234})
+
+
+#### 以下类型注意对应
+
+# def song(x,y):
+#     print(x,y)
+# 
+# song(**{'sdf':123,'sdfasdf':234})
+#
+# TypeError: song() got an unexpected keyword argument 'sdf'
