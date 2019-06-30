d91e34461e9342069d700b5762a5f79c9c72fb16
inThecompany <sxsong@cnpc>
2019-06-30 15:51:13 +0800

    gao jie function

--- 20190525New/20-day/4.generator.py
+++ 20190525New/20-day/4.generator.py
@@ -5,3 +5,34 @@
 # @File    : 4.generator.py
 
 
+### 生成器 可以用生成器 表达式 或者 yield 语句实现
+
+def gene(n):
+    for i in range(n):
+        yield i
+ll = gene(10)
+
+print(type(ll))
+for i in ll:
+    print('**',sep='  ',end=' ')
+print('')
+for i in ll:
+    print('*/*',sep='   ',end=' ')
+
+
+
+##############  区别 与 return ， yield 可以重复出现 且不影响
+
+
+def gene():
+    while True:
+        print('line 1')
+        yield 1
+        print('line 2')
+        yield 2
+        print('line 3')
+        yield 3
+ll = gene()
+
+for i in range(9):
+    next(ll)
