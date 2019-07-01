8bd51becc84cbb410d17c863f7b57c653f13636c
inThecompany <sxsong@cnpc>
2019-07-01 17:51:29 +0800

    20190701

--- 20190525New/21-day/1.高阶函数.py
+++ 20190525New/21-day/1.高阶函数.py
@@ -5,3 +5,25 @@
 # @File    : 1.高阶函数.py
 
 # 高阶函数的概念: 1 接受一个或3多个函数输入  2输出一个函数  满足下列两条之一
+
+
+def counter(base):
+    def inc(step=1):
+        nonlocal base
+        base += step
+        return base
+    return inc
+
+
+f1 = counter(10)
+f2 = counter(10)
+
+print(f1==f2)
+print(f1 is f2)
+print(f1() is f2())
+
+print(f1())
+print(f1())
+print(f1())
+print(f1())
+print(f2())
