7c7738afc93e571fcc8ff35a976655e494dc8eea
inThecompany <sxsong@cnpc>
2019-07-02 19:15:02 +0800

    wrapper copy property

--- 20190525New/22-day/1.functool.py
+++ 20190525New/22-day/1.functool.py
@@ -1,34 +1,27 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/7/1 23:19
+# @Time    :  2019/7/2 18:45
 # @Author  : songxy
-# @Email   : 2953xx998@qq.com
-# @File    : 2.带参数的修饰器.py
+# @Email   : ......998@qq.com
+# @File    : 1.functool.py
 
-
-### 柯里化  f(x+y) = f(x)(y)
-
+import functools
 import datetime
 
-def add(x):
-    def xxx(y):
-        return x+y
-    return xxx
-print(add(1)(2))
-
-
 def logger(fn):
-    def wapper(*args,**kwargs):
+    @functools.wraps(fn)   ##### 第二步
+    def zzq(*args,**kwargs):
+        """ this is a doc of zzq"""
         lstart = datetime.datetime.now()
         print('arg is {}, kwargs is {}'.format(args,kwargs))
         ret = fn(*args,**kwargs)
         lstop = datetime.datetime.now()
+        # functools.update_wrapper(zzq, fn)                     ### 第一步
         print('function {} tooks {}s'.format(fn.__name__,(lstop - lstart).total_seconds()))
-        wapper.__name__ = fn.__name__
         return ret
-    return wapper
+    return zzq
 @logger
 def add(x,y):
-    """sdfjlslfjajdfjalsfjja1111"""
+    """this is a doc of add"""
     return x+y
+print(add(11,66),add.__name__,add.__doc__)
 
-print(add(11,y=66),add.__name__,add.__doc__)
