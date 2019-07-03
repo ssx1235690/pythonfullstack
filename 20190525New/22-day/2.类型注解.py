173e8d31a114eb6e7accd1632054fd2e2da514e9
inThecompany <sxsong@cnpc>
2019-07-04 06:04:58 +0800

    类型注解

--- 20190525New/22-day/2.类型注解.py
+++ 20190525New/22-day/2.类型注解.py
@@ -3,3 +3,17 @@
 # @Author  : songxy
 # @Email   : ......998@qq.com
 # @File    : 2.类型注解.py
+
+
+def add(x:int,y:int):
+    """
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
+
+print(help(add))
+print(add(1,2))
+print(add.__annotations__)
+print(add.__annotations__['x'])
