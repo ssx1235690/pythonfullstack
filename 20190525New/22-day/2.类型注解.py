8bb44590f7077e2e851f4611cb87d7fef1fb6748
inThecompany <sxsong@cnpc>
2019-07-09 20:35:08 +0800

    free

--- 20190525New/22-day/2.类型注解.py
+++ 20190525New/22-day/2.类型注解.py
@@ -17,3 +17,17 @@ print(help(add))
 print(add(1,2))
 print(add.__annotations__)
 print(add.__annotations__['x'])
+print(type(add.__annotations__['x']))
+
+#### 为了不改变python 动态语言的性质，没有对参数的类型作强制判断
+
+from inspect import  signature
+
+
+def foo(a, *, b:int, **kwargs):
+    pass+
+sig = signature(foo)
+print(str(sig))
+
+
+print(str(sig.parameters['b']))
