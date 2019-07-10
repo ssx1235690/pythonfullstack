9a4c8fdd99d33476e78f4fa334323ab5f52fa3a4
inThecompany <sxsong@cnpc>
2019-07-10 20:36:00 +0800

    0710

--- 20190525New/21-day/2.带参数的修饰器.py
+++ 20190525New/21-day/2.带参数的修饰器.py
@@ -8,6 +8,10 @@
 ### 柯里化  f(x+y) = f(x)(y)
 
 import datetime
+import time
+import  random
+from multiprocessing import  Process
+from functools import  wraps
 
 def add(x):
     def xxx(y):
@@ -31,4 +35,35 @@ def add(x,y):
     """sdfjlslfjajdfjalsfjja1111"""
     return x+y
 
-print(add(11,y=66),add.__name__,add.__doc__)
+# print(add(11,y=66),add.__name__,add.__doc__)
+
+
+
+lsum = []
+
+def test(fn):
+    @wraps(fn)
+    def inner(n):
+        # time.sleep(random.randint(10,50))
+        print('ok')
+        fn(n)
+    return inner
+@test
+def feibo2(n):
+    for i in range(0,n):
+        if i == 0 or i == 1:
+            lsum.append(1)
+        else:
+            lsum.append(lsum[i-2]+lsum[i-1])
+    # print(lsum)
+    return lsum
+feibo2(10)
+
+if __name__ == '__main__':
+    processes = list()
+    for i in range(10):
+        p1 = Process(target=feibo2, args=(30000,))
+        p1.start()
+        processes.append(p1)
+    for p in processes:
+        p.join()
