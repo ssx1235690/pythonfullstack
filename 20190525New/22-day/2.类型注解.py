380ccce4155336b6e60fdf377639b2ccba8b0f66
song-pc <295312998@qq.com>
2019-07-10 23:41:25 +0800

    inspect functools

--- 20190525New/22-day/2.类型注解.py
+++ 20190525New/22-day/2.类型注解.py
@@ -21,13 +21,60 @@ print(type(add.__annotations__['x']))
 
 #### 为了不改变python 动态语言的性质，没有对参数的类型作强制判断
 
+
+# inspect 向内看 检测函数的所有参数
+
 from inspect import  signature
+import inspect
 
 
 def foo(a, *, b:int, **kwargs):
-    pass+
+    pass
 sig = signature(foo)
 print(str(sig))
 
 
 print(str(sig.parameters['b']))
+
+
+def a(a, b=0, *c, d, e=1, **f):
+    pass
+
+aa = inspect.signature(a)
+print("inspect.signature(fn)是:%s" % aa)
+print("inspect.signature(fn)的类型：%s" % (type(aa)))
+print("\n")
+
+bb = aa.parameters
+print("signature.paramerters属性是:%s" % bb)
+print("ignature.paramerters属性的类型是%s" % type(bb))
+print("\n")
+
+
+for i,items in enumerate(bb.items()):
+    param = items[1]
+    print(i,param.default,param.empty,param.kind,param.annotation)
+
+
+
+# 使用实例
+
+
+def check(fn):
+    def inner(*args,**kwargs):
+        sig = inspect.signature(fn)
+        param = list(sig.parameters.values())
+        for i,valu in enumerate(args):
+            if isinstance(valu, param[i].annotation):
+                print('ok')
+            else:
+                raise TypeError
+
+        return fn(*args,**kwargs)
+    return inner
+
+
+@check
+def add(x:int,y:int) -> int:
+    return x+y
+print(add(22,'dsdfsf'))
---
+++ 20190525New/22-day/3.partial.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/10 23:38
+# @Author  : songxy
+# @Email   : 2953xx998@qq.com
+# @File    : 3.partial.py
+
+
