ddc2b114a75930c6dcd6301fa46547e81c7888ee
song-pc <295312998@qq.com>
2019-07-14 20:15:08 +0800

    gong gong substr

--- 20190525New/23-day/1.字典扁平化.py
+++ 20190525New/23-day/1.字典扁平化.py
@@ -4,3 +4,26 @@
 # @Email   : ......998@qq.com
 # @File    : 1.字典扁平化.py
 
+
+
+# 源字典  {‘a’:{‘b’:1,‘c’:2},‘d’:{‘e’:3,‘f’:{‘g’:4}}}
+# 目标字典   {'d.e': 3, 'a.c': 2, 'd.f.g': 4, 'a.b': 1}
+
+
+source_dict = {'a':{'b':1,'c':2},'d':{'e':3,'f':{'g':4}}}
+
+target_dict = {}
+
+def flat(xxx,prefix=''):
+    for k,v in xxx.items():
+        if isinstance(v,dict):
+            prefix += k+'.'
+            flat(v,prefix=prefix)
+        else:
+            k = prefix + k
+            target_dict[k] = v
+
+    return target_dict
+print(flat(source_dict),'xxxxxx',source_dict)
+
+
---
+++ 20190525New/23-day/2.公共字串.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/14 17:14
+# @Author  : songxy
+# @Email   : 2953xx998@qq.com
+# @File    : 2.公共字串.py
+
+
+# 求最长公共字串
+
+s1 = 'asdfhauiwjbabcdfvbaiuasdfaskjdfnlhafnsjknfjkaiweasd,fnkanfkkafsefd'
+s2 = 'aklsnfuinvjasdfkkalksnfoefnasdkfnasdfxnuabcihvuzcjkniewu'
+
+
+
+#### 矩阵算法
+
+s2_index = 0
+maxth = 0
+matrix = []
+for i,val1 in enumerate(s1):
+    matrix.append([])
+    for j,val2 in enumerate(s2):
+        if val1 != val2:
+            matrix[i].append(0)
+        else:
+            if i == 0 or j == 0:
+               matrix[i].append(1)
+            else:
+                matrix[i].append(matrix[i-1][j-1] + 1)
+
+            if matrix[i][j] > maxth:
+                maxth = matrix[i][j]
+                s2_index = j + 1
+
+
+print(s2[s2_index - maxth:s2_index])
+
