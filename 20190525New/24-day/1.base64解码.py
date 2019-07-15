8bab47382a8f31d12fd43e94bea09999bf79a465
inThecompany <sxsong@cnpc>
2019-07-15 14:46:13 +0800

    base64

--- 20190525New/24-day/1.base64解码.py
+++ 20190525New/24-day/1.base64解码.py
@@ -7,26 +7,57 @@
 
 #使用 可变字节类型
 
-alph = b'jfaoiweronaoinfoiaisfoiaAKJIHIOHIFkhfshAHsJH/+'
+import base64
+
+alphbet = b'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'    #源码表
+
+alph = b'jfaoiweronaoinfoiaisfoiaAKJIHIOHIFkhfshAHsJH/+'           #待转换二进制
 
 
 def base64decode(src:bytes):
-    ret = bytearray()
+    ret = bytearray()    # 最终转换目标
     length = len(src)
 
     r = 0
     step = 3
-    tripe = bytes()
+
     for offset in  range(0,length,step):
         if offset + step <= length:
-            tripe += src[offset:offset+3]
+            tripe = src[offset:offset+3]
         else:
-            tripe += src[offset:]
+            tripe = src[offset:]
             r = 3 - len(tripe)
-            tripe += r*b'\x00'
+            tripe += b'\x00'*r
 
 
-    b = int.from_bytes(tripe,'big')
-    print(hex(b))
+        b = int.from_bytes(tripe,'big') # 大端模式  网络传输默认为大端模式  window 内部为小端模式
+        # f = b.to_bytes(1000,'big')
+        # print(f)
+        # print(b)
+        for i in range(18,-1,-6):
+            index = b >> i & 0x3F
+            ret.append(alphbet[index])
+        for i in range(1,r+1):
+            ret[-i] = 0x3D
+    return ret
 
-base64decode(alph)
+
+print(base64decode(alph))
+
+########## 使用原生  base64 的方法进行转码验证
+
+zz = base64decode(alph)
+ll = base64.decodebytes(zz)
+print(ll)
+#
+# # song = b'abc'
+# # c = int.from_bytes(song,'big')
+# # print(c >> 18)
+# # print(song[18])
+#
+# song = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# zz = ''
+# for i in song:
+#     zz += i
+# print(zz)
+# print(zz.upper())
