60314fccd487644d7ef65b7190e6bdf09df03898
song-pc <295312998@qq.com>
2019-07-18 23:37:19 +0800

    suanfa

---
+++ 20190525New/25-day/1.习题copy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/18 23:26
+# @Author  : songxy
+# @Email   : 2953xx998@qq.com
+# @File    : 1.习题copy.py
+
+
+src = 'song1.txt'
+dest = 'song2.txt'
+
+xx = open(src,'w',encoding='utf8')
+xx.write('卢卡斯京东方垃圾但是覅爱哦二级覅哦算法ja')
+xx.close()
+
+def copy(src,dest):
+    with open(src,'r+',encoding='utf8') as x1:
+        with open(dest,'w+',encoding='utf8') as x2 :
+            x2.write(x1.read())
+
+
+copy(src,dest)
