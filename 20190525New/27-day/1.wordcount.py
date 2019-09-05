2ff152648c733264c05b119004934ac3e71a9093
inThecompany <sxsong@cnpc>
2019-09-05 13:53:38 +0800

    word count

---
+++ 20190525New/27-day/1.wordcount.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2019/8/19 10:07
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 1.wordcount.py
+
+
+
+
+Str = '''
+Beijing ABT Networks Co., Ltd. will be listed on Shanghai Stock Exchange's sci-tech innovation board, commonly known as SSE STAR Market, the China Securities Regulatory Commission (CSRC) said in a statement over the weekend.
+
+It did not specify the total amount of funds to be raised.
+
+The company and its underwriter will confirm the IPO date and publish its prospectus following discussions with the stock exchange.
+
+The SSE STAR Market, inaugurated on June 13 and designed to focus on companies in the high-tech and strategic emerging sectors, will ease the listing criteria, such as allowing firms that have yet to make a profit to list, but they will adopt higher requirements for information disclosure.
+'''
+
+
+def wordcount(x):
+
+
