62141ac73d1298f93a41133d3c8e2f5b882b74b6
ssx <songshenxiangxxxx@ipalfish.com>
2019-09-27 21:58:31 +0800

    2019.927

--- 20190525New/27-day/1.wordcount.py
+++ 20190525New/27-day/1.wordcount.py
@@ -5,6 +5,7 @@
 # @File    : 1.wordcount.py
 
 
+import re
 
 
 Str = '''
@@ -16,8 +17,32 @@ The company and its underwriter will confirm the IPO date and publish its prospe
 
 The SSE STAR Market, inaugurated on June 13 and designed to focus on companies in the high-tech and strategic emerging sectors, will ease the listing criteria, such as allowing firms that have yet to make a profit to list, but they will adopt higher requirements for information disclosure.
 '''
+flag = 0
+word_dict = {}
+def makekeys(strxx):
+    for i in range(len(strxx)):
+
+        if flag == 0:
+            start = i
+
+        if i == 0 or re.match('\w',strxx[i]):
+            continue
+        elif  re.match('\W',strxx[i]) and re.match('\w',strxx[i-1]):
+            word_dict[strxx[start,i-1]] += 1
+        i += 1
+    return  word_dict
 
 
-def wordcount(x):
 
 
+def wordcount():
+    """
+    进行词量统计的一个函数
+    原理  分词 ---- 组词典 -----sort  ---返回 top5
+    :return:
+    """
+    pass
+
+
+
+print(makekeys(Str))
---
+++ work/aliyunxx.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2019/9/16
+# @Author  : user01
+# @Email   : ......998@qq.com
+# @File    : aliyunxx
+
+a, *b = 1,2,3,4,5
+print(a,b)
+
+
+a, b =  b, a
+
+print(a,b)
