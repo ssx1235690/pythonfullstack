b93125eb60d43d665a3f14fd3bab2672486fc5d2
sxsong <29xxxx998@qq.com>
2019-11-20 23:52:37 +0800

    32-day

--- 20190525New/31-day/3.practise_class.py
+++ 20190525New/31-day/3.practise_class.py
@@ -5,3 +5,13 @@
 # @File    : 3.practise_class
 
 
+class Animal():
+    _xx1  = 22
+    __xxx2 = 222
+    def __init__(self):
+        self._xx3 = 33
+        self.__xxx3 = 333
+
+cat = Animal()
+print(Animal.__dict__)
+print(cat.__dict__)
---
+++ 20190525New/32-day/1.classinherit.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2019/11/20
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 1.classinherit
+
