2cbb9066f8fbfc99b43cc09d8640d51ff8253160
sxsong <29xxxx998@qq.com>
2019-11-21 00:05:33 +0800

    32-day-1

--- 20190525New/32-day/1.classinherit.py
+++ 20190525New/32-day/1.classinherit.py
@@ -4,3 +4,20 @@
 # @Email   : ......998@qq.com
 # @File    : 1.classinherit
 
+class A:
+    __classA = 100
+    def spam(self):
+        print('A.spam')
+
+class B(A):
+    def spam(self):
+        print('B.spam')
+        super().spam()  # Call parent spam()
+
+print(B.__base__)
+print(B.__bases__)
+print(B.__mro__)
+print(B.__subclasses__)
+
+print(A.__dict__)
+print(B.__dict__)
