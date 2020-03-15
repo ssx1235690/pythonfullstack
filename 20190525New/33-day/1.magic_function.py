d7a3af4cdf9b86a71bb76c602b15b0ed61d72577
sxsong <29xxxx998@qq.com>
2020-03-15 22:53:07 +0800

    hash

--- 20190525New/33-day/1.magic_function.py
+++ 20190525New/33-day/1.magic_function.py
@@ -4,9 +4,49 @@
 # @Email   : ......998@qq.com
 # @File    : 1.magic_function
 
+def area(width, height):
+    return width * height
 
-class A: pass
+
+class A:
+    '''
+    sdfhajkksdfjasdfasdf
+    '''
+    pass
+print(A.__name__)  #A 是表示符   __name__ 才是真正的变量值 字符串
 
 print(A.__module__)
 print(A().__module__)
+
+# A的类型
+print(A.__class__)
+
+# A的基类
+print(A.__bases__)
+
+# A 的注释
+print(A.__doc__)
+
+# 类的继承顺序
+print(A.__mro__)
+
+# 字典
+
+print(A.__dict__)
+
+
+# 收集当前模块的所有值
 print(dir())
+
+print(dir(A))
+print(dir(area))
+
+
+# 魔术方法
+
+# 创建 和 销毁
+
+# init  del
+
+
+
---
+++ 20190525New/33-day/2.hash和eq.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2020/3/15
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 2.hash和eq
+
