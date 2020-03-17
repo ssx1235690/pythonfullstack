49db4f2710e32a26841ca0fb200bd61ad4f4bd1f
sxsong <29xxxx998@qq.com>
2020-03-18 00:13:06 +0800

    word count

--- 20190525New/33-day/4.bool和可视化.py
+++ 20190525New/33-day/4.bool和可视化.py
@@ -4,7 +4,7 @@
 # @Email   : ......998@qq.com
 # @File    : 4.bool和可视化
 
-
+import json
 class MyClass:
     """一个简单的类实例"""
     i = 12345
@@ -15,10 +15,41 @@ class MyClass:
         return 'hello world'
     def __bool__(self):
         # return False
-        return False
+        return True
 
 
 print(dir(MyClass))
 
 if MyClass(1,2):
     print('bool  is  ok')
+
+
+
+'''
+可视化
+'''
+
+
+list1 = [1,2,3,4,5,]
+print(*list1)
+
+
+class MyClass:
+    """一个简单的类实例"""
+    i = 12345
+    def __init__(self):
+        self.c = 1
+
+    def f(self):
+        return 'hello world'
+    def  __repr__(self):
+        return 'repr output '
+    def __str__(self):
+        return 'songshenxiang'
+    def __bytes__(self):
+        return json.dumps([123,123]).encode()
+
+print(MyClass())
+print([MyClass()])
+print([str(MyClass())])
+print(bytes(MyClass()))
---
+++ 20190525New/33-day/5.运算符.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    :  2020/3/18
+# @Author  : songxy
+# @Email   : ......998@qq.com
+# @File    : 5.运算符
+
+
+class MyClass:
+    """一个简单的类实例"""
+    i = 12345
+    def __init__(self):
+        self.c = MyClass.i
+
+    def f(self):
+        return 'hello world'
+    def __bool__(self):
+        # return False
+        return True
+
+print(dir(MyClass()))
