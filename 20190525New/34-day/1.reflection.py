225d818dbb847e4464e05ccecda91dbdc6dd7965
sxsong <29xxxx998@qq.com>
2020-08-24 09:26:40 +0800

    reflection context

--- 20190525New/34-day/1.reflection.py
+++ 20190525New/34-day/1.reflection.py
@@ -31,4 +31,37 @@ import functools
 
 setattr(functools,'lele','niubi')
 ll = getattr(functools,'lele')
-print(ll)
+# print(ll)
+
+
+
+
+########## getattr 对实例级别的异常进行控制
+
+class  Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __getattr__(self, item):
+        print(4,item,'------------------------------')
+        print(5,type(item),'~~~~~~~~~~~~~~~~~~~~~')
+        return 123456
+    def __setattr__(self, key, value):
+        print('setattr',key,value)
+        # self.key = value 会引起递归循环
+        self.__dict__[key] = value
+p = Point(4,5)
+print(1,p)
+print(2,p.x,p.y)
+print(3,p.n)
+
+
+
+### 如果实例中存在相应属性那么不会引起 getattr 属性调用， 使用 dict 方式进行赋值不会调用setaatr 方法
+
+print(p.yy)
+p.__dict__['yy']  = 1000
+
+print(p.zz)
+p.zz = 100000
+print(p.zz)
